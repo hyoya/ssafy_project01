cee58e246c3e8349b341998f331c8d2e7f0503a4
kim <kkt3526@naver.com>
2019-01-31 14:03:19 +0900

    test2

---
+++ SafyFood/src/com/ssafy/FoodSAX.java
@@ -0,0 +1,72 @@
+package com.ssafy;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class FoodSAX {
+	List<Foods> list = new ArrayList<>();
+	
+	public List<Foods> getFoodsList(String url){
+		connectFoods(url);
+		return list;
+	}
+	public void connectFoods(String url) {
+		SAXParserFactory f = null;
+		try {
+			f = SAXParserFactory.newInstance();
+			SAXParser p = f.newSAXParser();
+			p.parse(new URL(url).openConnection().getInputStream(), new SAXHandler());
+		} catch (SAXException | IOException e) {
+			System.out.println(e);
+		} catch (ParserConfigurationException e) {
+			System.out.println(e);
+		}
+	}
+	class SAXHandler extends DefaultHandler{
+		StringBuilder b = new StringBuilder();
+		boolean flag = false;
+		Foods food = null;
+		@Override
+		public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+			if(qName.equalsIgnoreCase("item")) {
+				food = new Foods();
+				flag = true;
+			}
+		}
+		@Override
+		public void endElement(String uri, String localName, String qName) throws SAXException {
+			if(flag && food != null) {
+				switch(qName) {
+				case "prdlstReportNo": food.setFoodcode(b.toString().trim());
+					break;
+				case "prdlstNm": food.setFoodname(b.toString().trim());
+					break;
+				case "manufacture": food.setMaker(b.toString().trim());
+					break;
+				case "rawmtrl": food.setMaterial(b.toString().trim());
+					break;
+				case "item":
+					list.add(food);
+				}
+			}
+		}
+		@Override
+		public void characters(char[] ch, int start, int length) throws SAXException {
+			b.setLength(0);
+			b.append(ch,start,length);
+		}
+		
+		
+	}
+}
+
---
+++ SafyFood/src/com/ssafy/Foods.java
@@ -0,0 +1,55 @@
+package com.ssafy;
+
+public class Foods {
+	
+	private String foodcode;
+	private String foodname;
+	private String maker;
+	private String material;
+
+	
+	public Foods(String foodcode, String foodname, String maker, String material) {
+		this.foodcode = foodcode;
+		this.foodname = foodname;
+		this.maker = maker;
+		this.material = material;
+	}
+	
+	public Foods() {
+		
+	}
+
+	public String getFoodcode() {
+		return foodcode;
+	}
+
+	public void setFoodcode(String foodcode) {
+		this.foodcode = foodcode;
+	}
+
+	public String getFoodname() {
+		return foodname;
+	}
+
+	public void setFoodname(String foodname) {
+		this.foodname = foodname;
+	}
+
+	public String getMaker() {
+		return maker;
+	}
+
+	public void setMaker(String maker) {
+		this.maker = maker;
+	}
+
+	public String getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+
+	
+}
