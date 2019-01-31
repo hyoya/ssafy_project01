9c1844215b6b45750d403ff281d1ec8244f1a969
kim <kkt3526@naver.com>
2019-01-31 12:35:32 +0900

    test

--- SafyFood/src/com/ssafy/IntroPage.java
+++ SafyFood/src/com/ssafy/IntroPage.java
@@ -1,5 +1,63 @@
 package com.ssafy;
 
-public class IntroPage {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class IntroPage implements ActionListener{
+	private JFrame f1;
+	private JLabel title;
+	private JPanel panel;
+	private JButton b1;
+	
+	
+	public IntroPage() {
+		createFrame1GUI();
+		addEvent();
+	}
+	private void addEvent() {
+		b1.addActionListener(this);
+		
+	}
+	public void createFrame1GUI() {
+		f1 = new JFrame();
+		panel = new JPanel();
+		b1 = new JButton("검색");
+		
+		f1.setLayout(new BorderLayout());
+		
+		title = new JLabel("SAFE FOOD");
+		title.setFont(new Font("Serif",Font.BOLD,50));
+		title.setForeground(Color.RED);
+		title.setHorizontalAlignment(SwingConstants.CENTER);
+	
+		f1.add(title,"North");
+		
+		panel.setLayout(new FlowLayout());
+		panel.add(b1);
+		
+		f1.add(panel);
+		
+		f1.setSize(400,200);
+		f1.setVisible(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == b1) {
+			SearchPage f2 = new SearchPage();
+			f1.setVisible(false);
+		}
+		
+	}
+	
 }
---
+++ SafyFood/src/com/ssafy/SafeFoodMain.java
@@ -0,0 +1,10 @@
+package com.ssafy;
+
+public class SafeFoodMain {
+
+	public static void main(String[] args) {
+		IntroPage i = new IntroPage();
+
+	}
+
+}
